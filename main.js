b71e6baa0681277543499f8547f754c8a5afbfb8
telalim1988 <ta.elalim@gmail.com>
2026-04-12 18:39:23 +0400

    Update main.js

--- main.js
+++ main.js
@@ -362,6 +362,8 @@ document.getElementById("recommendation").innerText =
   
 document.getElementById("energy").innerText = energy.toFixed(2);
 document.getElementById("cost").innerText = cost.toFixed(2);
+document.getElementById("ai_result").innerText =
+  "Save up to " + ((cost - best_cost)/cost * 100).toFixed(1) + "% energy";
   
 document.getElementById("pump_flow").innerText = flow_pump.toFixed(2);
 }
